6c5cf4e22734438e8f0bd2e62a5b62f176743c92
hojun <songhojun@gmail.com>
2021-04-05 16:55:29 +0900

    .

---
+++ build/index.js
@@ -0,0 +1,36 @@
+import {helloWorld} from './hello-world.js'
+import confetti from './_snowpack/pkg/canvas-confetti.js'
+import io from "./_snowpack/pkg/socket.io-client.js"
+import * as THREE from './_snowpack/pkg/three.js';
+
+
+const socket = io()
+
+helloWorld()
+
+confetti.create(document.getElementById('canvas'), {
+  resize: true,
+  useWorker: true,
+ })({ particleCount: 200, spread: 200 })
+ 
+
+socket.on("currentcolor", function (data) {
+    if (data) {
+      let redVal = Math.floor(map_range(parseInt(data),0,1023,0,255))
+      document.body.style.backgroundColor =  `rgb(${redVal},0,0)`
+      console.log("background color to: "+ redVal +", 0, 0");
+    } else {
+      console.log("error!");
+    }
+})
+
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
---
+++ server.js
@@ -0,0 +1,34 @@
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const port = new SerialPort('COM4', {baudRate: 9600})
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+
+const express = require('express')
+const app = new express()
+
+const http = require('http').createServer(app)
+
+const io = require('socket.io')(http)
+
+app.use(express.static(__dirname + "/build"))
+
+app.get("/", function (req, res, next) {
+  res.sendFile(__dirname + "/index.html")
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+port.on('open',function() {
+    console.log('port opened')
+  })
+
+parser.on('data', function(value) {
+    console.log(value)
+    io.emit("currentcolor", value);
+  })
